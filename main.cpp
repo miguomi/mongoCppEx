47a889179d5edb284111b1cb4fb46618d4fbd8b2
miguomi <2572889641@qq.com>
2020-03-31 02:51:35 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+#include <cstdlib>
+//#include <bsoncxx/v_noabi/bsoncxx/builder/basic/array.hpp>
+#include <bsoncxx/v_noabi/bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/v_noabi/bsoncxx/builder/basic/kvp.hpp>
+//#include<bsoncxx/builder/basic/sub_document.hpp>
+//#include<bsoncxx/v_noabi/bsoncxx/builder/basic/sub_document.hpp>
+
+
+int main(){
+
+    std::cout<<"hello"<<std::endl;
+    
+    bsoncxx::builder::basic::document doc = bsoncxx::builder::basic::document{};
+
+
+
+    return 0;
+}
